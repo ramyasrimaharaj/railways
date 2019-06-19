a3c6096240b5bf58e28f1b819acf4b93ca4929c3
ramya maharaj <ramyamaharaj74@gmail.com>
2019-06-19 11:26:33 +0530

    settings

--- project1/settings.py
+++ project1/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
+***********************i did nothing here****************
 """
 
 import os
